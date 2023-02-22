b59d81d4dbf5254f37124d18e636c810c763ca3a
Ygnas <Ygnas@outlook.com>
2023-02-22 12:28:03 +0000

    users own placemark

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,10 +1,14 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
+      const placemarks = await db.placemarkStore.getUserPlacemarks(loggedInUser._id);
       const viewData = {
         title: "Placemark Dashboard",
         user: loggedInUser,
+        placemarks: placemarks,
       };
       return h.view("dashboard-view", viewData);
     },
