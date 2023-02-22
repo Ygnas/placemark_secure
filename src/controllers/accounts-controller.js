6936d644ea33d71ccfac2f7ec12f7b3e8af292ca
Ygnas <Ygnas@outlook.com>
2023-02-22 14:23:46 +0000

    logout now clears cookies

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -62,6 +62,7 @@ export const accountsController = {
   logout: {
     auth: false,
     handler: function (request, h) {
+      request.cookieAuth.clear();
       return h.redirect("/");
     },
   },
