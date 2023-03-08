16ede113f7e8ef0bc9ba3eb533a89e562380c773
Ygnas <Ygnas@outlook.com>
2023-03-08 13:15:00 +0000

    category mongo models

---
+++ src/models/mongo/category-mongo-store.js
@@ -0,0 +1,43 @@
+import { Category } from "./category.js";
+import { placemarkMongoStore } from "./placemark-mongo-store.js";
+
+export const categoryMongoStore = {
+  async getAllCategorys() {
+    const categorys = await Category.find().lean();
+    return categorys;
+  },
+
+  async getCategoryById(id) {
+    if (id) {
+      const category = await Category.findOne({ _id: id }).lean();
+      if (category) {
+        category.placemarks = await placemarkMongoStore.getPlacemarkByCategoryId(category._id);
+      }
+      return category;
+    }
+    return null;
+  },
+
+  async addCategory(category) {
+    const newCategory = new Category(category);
+    const categoryObj = await newCategory.save();
+    return this.getCategoryById(categoryObj._id);
+  },
+
+  async getUserCategory(id) {
+    const category = await Category.find({ userid: id }).lean();
+    return category;
+  },
+
+  async deleteCategoryById(id) {
+    try {
+      await Category.deleteOne({ _id: id });
+    } catch (error) {
+      console.log("bad id");
+    }
+  },
+
+  async deleteAllCategorys() {
+    await Category.deleteMany({});
+  }
+};
