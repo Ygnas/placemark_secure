9c1a36b5c6fdfe0144725ff6cfa4f1bbe3d7566d
Ygnas <Ygnas@outlook.com>
2023-03-08 20:20:11 +0000

    updated main server file

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
-import Hapi from "@hapi/hapi";
+import Inert from "@hapi/inert";
 import Vision from "@hapi/vision";
+import Hapi from "@hapi/hapi";
 import Cookie from "@hapi/cookie";
-import Handlebars from "handlebars";
 import dotenv from "dotenv";
 import path from "path";
 import Joi from "joi";
+import jwt from "hapi-auth-jwt2";
+import HapiSwagger from "hapi-swagger";
 import { fileURLToPath } from "url";
-import { accountsController } from "./controllers/accounts-controller.js";
+import Handlebars from "handlebars";
 import { webRoutes } from "./web-routes.js";
-import { apiRoutes } from "./api-routes.js";
 import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
+import { validate } from "./api/jwt-utils.js";
+import { apiRoutes } from "./api-routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,14 +24,41 @@ if (result.error) {
   process.exit(1);
 }
 
+const swaggerOptions = {
+  info: {
+    title: "Playtime API",
+    version: "0.1",
+  },
+  securityDefinitions: {
+    jwt: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header",
+    },
+  },
+  security: [{ jwt: [] }],
+};
+
 async function init() {
   const server = Hapi.server({
-    port: 3000,
     host: "localhost",
+    port: process.env.PORT || 3000,
   });
 
+  await server.register(Inert);
   await server.register(Vision);
   await server.register(Cookie);
+  await server.register(jwt);
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ]);
+
   server.validator(Joi);
 
   server.views({
@@ -51,8 +82,13 @@ async function init() {
     redirectTo: "/",
     validate: accountsController.validate,
   });
-
+  server.auth.strategy("jwt", "jwt", {
+    key: process.env.cookie_password,
+    validate: validate,
+    verifyOptions: { algorithms: ["HS256"] },
+  });
   server.auth.default("session");
+
   db.init();
   server.route(webRoutes);
   server.route(apiRoutes);
