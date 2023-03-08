c79ff053d97fd9e0086ff94d166ca727b1ce0059
Ygnas <Ygnas@outlook.com>
2023-03-08 13:14:31 +0000

    category controller

---
+++ src/controllers/category-controller.js
@@ -0,0 +1,45 @@
+import { PlacemarkSpec } from "../models/joi-schemas.js";
+import { db } from "../models/db.js";
+
+export const categoryController = {
+  index: {
+    handler: async function (request, h) {
+      const category = await db.categoryStore.getCategoryById(request.params.id);
+      const viewData = {
+        title: "Category",
+        category: category,
+      };
+      return h.view("category-view", viewData);
+    },
+  },
+
+  addPlacemark: {
+    validate: {
+      payload: PlacemarkSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("category-view", { title: "Add placemark error", errors: error.details }).takeover().code(400);
+      },
+    },
+    handler: async function (request, h) {
+      const category = await db.categoryStore.getCategoryById(request.params.id);
+      console.log(category);
+      const newPlacemark = {
+        name: request.payload.name,
+        description: request.payload.description,
+        latitude: request.payload.latitude,
+        longitude: request.payload.longitude,
+      };
+      await db.placemarkStore.addPlacemark(category._id, newPlacemark);
+      return h.redirect(`/category/${category._id}`);
+    },
+  },
+
+  deletePlacemark: {
+    handler: async function (request, h) {
+      const placemark = await db.placemarkStore.getPlacemarkById(request.params.id);
+      await db.placemarkStore.deletePlacemark(request.params.placemarkid);
+      return h.redirect(`/placemark/${placemark._id}`);
+    },
+  },
+};
