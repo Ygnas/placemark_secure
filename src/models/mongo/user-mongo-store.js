30e23252ceb2085c705eb7943a6aa38037677d2c
Ygnas <Ygnas@outlook.com>
2023-03-13 21:17:10 +0000

    by default user is not an admin

--- src/models/mongo/user-mongo-store.js
+++ src/models/mongo/user-mongo-store.js
@@ -16,6 +16,7 @@ export const userMongoStore = {
 
   async addUser(user) {
     const newUser = new User(user);
+    newUser.admin = false;
     const userObj = await newUser.save();
     const u = await this.getUserById(userObj._id);
     return u;
